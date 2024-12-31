2a4182906f54bdc416c9969c081482385e142e71
sina koushki <sinaderik7@gmail.com>
2024-12-31 11:35:19 +0330

    next project is initialized

---
+++ app/components/Hello.jsx
@@ -0,0 +1,13 @@
+'use client '
+import React from "react";
+
+const Hello = () => {
+  console.log("Im  a client component");
+  return (
+    <div>
+      <h2>Hello user</h2>
+    </div>
+  );
+};
+
+export default Hello;
